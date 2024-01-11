7a3be1ec2f6538c8b24fb2e03cc751c38494bd67
BlackVedro <zykovvedro@gmail.com>
2024-01-12 00:05:30 +0300

    Initial Commit

---
+++ rock-paper-scissors!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rock-paper-scissors!
+//
+//  Created by Timofey on 12.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
