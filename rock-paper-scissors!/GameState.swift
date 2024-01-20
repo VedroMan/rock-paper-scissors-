9e4dd21b1eedcfe069f8f6386348bd8400201153
BlackVedro <zykovvedro@gmail.com>
2024-01-20 23:00:36 +0300

    Version 1, commit save

---
+++ rock-paper-scissors!/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  rock-paper-scissors!
+//
+//  Created by Timofey on 12.01.24.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw;
+}
+
---
+++ rock-paper-scissors!/Sign.swift
@@ -0,0 +1,73 @@
+//
+//  Sign.swift
+//  rock-paper-scissors!
+//
+//  Created by Timofey on 12.01.24.
+//
+
+import Foundation
+import GameplayKit
+
+let randomChoice = GKRandomDistribution(lowestValue: 0, highestValue: 2)
+
+func randomSign() -> Sign {
+    
+    let sign = randomChoice.nextInt()
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+    
+    enum Sign {
+        case rock, paper, scissors
+        
+        var emojiValue: String {
+            switch self {
+            case .paper:
+                return "📄"
+            case .rock:
+                return "🪨"
+            case .scissors:
+                return "✂️"
+            }
+        }
+        
+        func getResult (for oposite: Sign) -> GameState {
+            
+            switch self {
+            case .rock:
+                switch oposite {
+                case .rock:
+                    return .draw
+                case .paper:
+                    return .lose
+                case .scissors:
+                    return .win
+                }
+            case .paper:
+                switch oposite {
+                case .paper:
+                    return .draw
+                case .scissors:
+                    return .lose
+                case .rock:
+                    return .win
+                }
+            case .scissors:
+                switch oposite {
+                case .scissors:
+                    return .draw
+                case .rock:
+                    return .lose
+                case .paper:
+                    return .win
+                }
+            }
+            
+        }
+        
+    }
